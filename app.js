c739327bbf6e557f536ab0d12244407089972bed
MGuardia10 <mike.guardia0309@gmail.com>
2023-01-30 12:36:44 +0100

    webserver listo

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+// Servir contenido estático - Middlewares
+app.use( express.static('public') );
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Miguel Guardia',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/generic', (req, res) => {
+    res.render( 'generic', {
+        nombre: 'Miguel Guardia',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render( 'elements', {
+        nombre: 'Miguel Guardia',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hello World from hola-mundo')
+});
+
+// Se muestra siempre que no se encuentre una ruta
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
